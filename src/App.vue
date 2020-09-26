4e42bf1a833c2d2fc8d883156d25552a5113dec9
luyao <luyao@doublefs.com>
2020-09-26 19:04:12 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-09-26 12:06:17
+ * @LastEditTime: 2020-09-26 14:07:03
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /vue3-project/src/App.vue
+-->
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<router-view />
 </template>
 
 <style lang="less">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
     color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
